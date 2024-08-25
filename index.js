bfde4948dc5158e980fe281197ce1842ef00efc8
sumitFlutter <gohilsumit142@gmail.com>
2024-08-25 08:00:32 +0530

    1st Program

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello World!  ")
+console.log("My name is Sumit Gohil \nThis is my 1st JavaScript Program using Node Environment")
+console.log("Today we will do some basic experiment with js ")
+console.log("5+5 = "+5+5)
+console.log("(5+5) = "+(5+5))
+console.log("(5+'5') = "+5+'5')
+console.log("(5-'5') = "+(5-'5'))
